e6889a9cd3fa8b54f904c8625d7f47f88c67478f
Oscar <o.frykedal@gmail.com>
2025-09-25 13:05:38 +0200

    added left/right arrow switch on images

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const gallery = document.querySelector(".gallery");
 const lightbox = document.getElementById("lightbox");
 const lightboxImage = document.querySelector(".lightboxImage");
 const closeBtn = document.querySelector(".closeBtn");
+let nextPhoto = 0;
 
 gallery.addEventListener("click", function (event) {
   const clickedPhoto = event.target.src;
+
   if (clickedPhoto) {
     lightboxImage.src = clickedPhoto;
     lightbox.classList.add("active");
@@ -13,20 +15,38 @@ gallery.addEventListener("click", function (event) {
 
 // CLOSE BUTTON
 closeBtn.addEventListener("click", function () {
-  console.log("CLICK BUTTON");
   lightbox.classList.remove("active");
 });
 
 // CLOSE WHEN CLICKING ANYWHERE
 lightbox.addEventListener("click", function (event) {
-  console.log("CLICK ANYWHERE");
   lightbox.classList.remove("active");
 });
 
-// CLOSE WITH ESC
+// CLOSE WITH ESC KEY AND GO LEFT/RIGHT WITH ARROW KEYS
 document.addEventListener("keydown", function (event) {
   if (event.key === "Escape" && lightbox.classList.contains("active")) {
-    console.log("ESCAPE");
+    // console.log("ESCAPE");
     lightbox.classList.remove("active");
+  }
+
+  if (event.key === "ArrowRight" && lightbox.classList.contains("active")) {
+    // console.log("RIGHT");
+    if (nextPhoto >= gallery.children.length - 1) {
+      nextPhoto = 0;
+    } else {
+      nextPhoto++;
+    }
+    lightboxImage.src = gallery.children[nextPhoto].children[0].src; // Example logic to go to the next image
+  }
+
+  if (event.key === "ArrowLeft" && lightbox.classList.contains("active")) {
+    // console.log("LEFT");
+    if (nextPhoto <= [0]) {
+      nextPhoto = [gallery.children.length - 1];
+    } else {
+      nextPhoto--;
+    }
+    lightboxImage.src = gallery.children[nextPhoto].children[0].src; // Example logic to go to the next image
   }
 });
